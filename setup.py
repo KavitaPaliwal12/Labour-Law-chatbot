a8ce9174a8b19e3dc9ad3460514e1ab65cd20201
KavitaPaliwal12 <paliwalkavita27@gmail.com>
2025-05-26 22:17:01 +0530

    labourlaw chatbox

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AT Project',
+    version = '0.0.0',
+    author = 'Kavita Paliwal',
+    author_email = 'paliwalkavita27@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+
+)
